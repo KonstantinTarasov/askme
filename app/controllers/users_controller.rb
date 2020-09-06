6875a38bcf50061a0d04259c87c51734efb7b9cf
SSmitt <lalaproger@yandex.ru>
2020-09-06 21:56:16 +0500

    Updated schema and added part 2 of the view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,5 +15,11 @@ class UsersController < ApplicationController
         avatar_url: 'https://secure.gravatar.com/avatar/' \
           '71269686e0f757ddb4f73614f43ae445?s=100'
     )
+
+    @questions = [
+        Question.new(text: 'Как дела?', created_at: Date.parse('06.09.2020')),
+        Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('06.08.2020'))
+    ]
+
   end
 end
