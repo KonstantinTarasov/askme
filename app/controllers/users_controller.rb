1962443b8527ae152ccb4ac808f455e9ae5cb5c8
SSmitt <lalaproger@yandex.ru>
2020-09-07 19:28:32 +0500

    Views refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
   def index
+    # Мы создаем массив из двух болванок пользователей. Для создания фейковой
+    # модели мы просто вызываем метод User.new, который создает модель, не
+    # записывая её в базу.
+    @users = [
+        User.new(
+            id: 1,
+            name: 'Vadim',
+            username: 'installero',
+            avatar_url: 'https://secure.gravatar.com/avatar/' \
+          '71269686e0f757ddb4f73614f43ae445?s=100'
+        ),
+        User.new(id: 2, name: 'Misha', username: 'aristofun')
+    ]
   end
 
   def new
@@ -21,5 +34,6 @@ class UsersController < ApplicationController
         Question.new(text: 'В чем смысл жизни?', created_at: Date.parse('06.08.2020'))
     ]
 
+    @new_question = Question.new
   end
 end
