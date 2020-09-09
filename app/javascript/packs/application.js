94f31891070361a9950d36c9fe3e5a12f2e67b2d
SSmitt <lalaproger@yandex.ru>
2020-09-09 23:20:31 +0500

    Added missing styles in the users file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,6 +16,7 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
 $(function(){
     $('#ask-button').click(function(){
         $('#ask-form').slideToggle(300);
