1a487188db55d931f6addfc46fa7fc8e24788640
SSmitt <lalaproger@yandex.ru>
2020-09-19 23:15:53 +0500

    Сhanged application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,17 @@ module ApplicationHelper
   def fa_icon(icon_class)
     content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
+
+  # Хелпер, необходимый считающий количество вопросов
+  def number_questions(number, enot, enota, enotov)
+    ostatok = number % 10
+
+    return enotov if number.between?(11, 14) || (number % 100).between?(11, 14)
+
+    return enot if ostatok == 1
+
+    return enota if ostatok.between?(2, 4)
+
+    return enotov if ostatok.between?(5, 9) || ostatok == 0
+  end
 end
