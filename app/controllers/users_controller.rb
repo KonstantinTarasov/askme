cb54578279d65f6a2e7ca57c13f965251e8992e4
SSmitt <lalaproger@yandex.ru>
2020-09-28 19:06:51 +0500

    User registration changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_url, notice: 'Пользователь успешно зарегистрирован'
+      session[:user_id] = @user.id
+      redirect_to user_path(@user), notice: 'Вы успешно залогинились!'
     else
       render 'new'
     end
