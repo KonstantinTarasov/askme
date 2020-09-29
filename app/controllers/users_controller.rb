3c809f3ae4aeb2bd4e5f0f2a0391d43167bc77bc
SSmitt <lalaproger@yandex.ru>
2020-09-29 15:57:19 +0500

    Fixed suffixes for redirects in controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,12 +8,12 @@ class UsersController < ApplicationController
   end
 
   def new
-    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    redirect_to root_path, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new
   end
 
   def create
-    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+    redirect_to root_path, alert: 'Вы уже залогинены' if current_user.present?
 
     @user = User.new(user_params)
 
