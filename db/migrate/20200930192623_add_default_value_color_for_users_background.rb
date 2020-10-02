695b1b0ed5d472f1868e73fa7800dfa0d18c936c
SSmitt <lalaproger@yandex.ru>
2020-10-02 22:17:07 +0500

    Deleted migration files

--- db/migrate/20200930192623_add_default_value_color_for_users_background.rb
+++
@@ -1,5 +0,0 @@
-class AddDefaultValueColorForUsersBackground < ActiveRecord::Migration[6.0]
-  def change
-    change_column :users, :background, :string, default: '#005a55'
-  end
-end
--- db/migrate/20200930213330_add_value_color_for_users_background.rb
+++
@@ -1,5 +0,0 @@
-class AddValueColorForUsersBackground < ActiveRecord::Migration[6.0]
-  def change
-    change_column :users, :background, :string
-  end
-end
