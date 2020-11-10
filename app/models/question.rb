f37f7b681d46072a070875477e1fd26fe58b9821
SSmitt <lalaproger@yandex.ru>
2020-11-10 22:55:23 +0500

    Add hastags controller

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,22 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
 
+  has_many :hashtag_questions, dependent: :destroy
+  has_many :hashtags, through: :hashtag_questions
+
   validates :text, presence: true
   validates :text, length: { maximum: 255}
+
+  after_save_commit :create_hashtags
+
+  private
+
+  def create_hashtags
+    self.hashtags =
+        "#{answer} #{text}".
+            downcase.
+            scan(Hashtag::REGEXP).
+            uniq.
+            map { |ht| Hashtag.find_or_create_by(text: ht.delete('#')) }
+  end
 end
